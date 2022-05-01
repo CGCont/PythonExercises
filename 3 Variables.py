f2705068ced5a37fae008929bbf1856e471c2f38
Gerardo Contreras <cgcontreras23@gmail.com>
2022-05-01 18:28:06 -0500

    Se añade archivo de variables

---
+++ 3 Variables.py
@@ -0,0 +1,12 @@
+name = "World"
+
+x, book = 100, "I Robot"
+print(x)
+print(book)
+print(x, book)
+
+#Conveciones par anombrar variables
+book_name = "Origin" #Snake Case
+bookName = "El libro negro de la nueva izquierda" #Camel Case
+BookName = "La batalla cultural" #Pascal Case
+PI = 3.1416 #Concención para constantes
