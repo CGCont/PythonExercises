ce826da34a81683617c79282657ed2634ebd7ddd
Gerardo Contreras <cgcontreras23@gmail.com>
2022-05-01 18:15:19 -0500

    Se añade archivo con la descripción de algunos tipos de datos

---
+++ 2 Datatypes.py
@@ -0,0 +1,32 @@
+#Strings
+print("Hello world")
+type("Hello world") #obtener tipo de datos
+print("Bye" + "world")
+
+#Integer
+print(30)
+
+#Float
+print(20.5)
+
+#Boolean
+True
+False
+
+#Lists
+[10,20,30,55]
+["José","María","Pedro"]
+
+#Tuples listas que no cambian
+(10,20,30,55)
+
+#Dictionaries
+{
+    "Name": "Ryan",
+    "Last_name": "Ray",
+    "Nick_name": "Fews"
+}
+
+#None type
+None
+
