adc31a49ca6a36b9cdea4ce7bf480bf8ef11f83e
Gerardo Contreras <cgcontreras23@gmail.com>
2022-05-06 23:18:41 -0500

    Agregar archivo con números

---
+++ 5 Number.py
@@ -0,0 +1,9 @@
+print(type(9))
+print(type(9.3))
+
+print(2**3) #Potencia 2 al cubo
+print(3 // 2 ) #Devuelve la parte entera de la divisón
+print(4 % 5 ) #Devuelve el modulo (residuo)
+age = input("Insert your age: ")
+new_age = int(age) + 5
+print(new_age)
