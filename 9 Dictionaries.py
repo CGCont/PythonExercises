ad2b537e8ff536718466803c40c10921b667b472
Gerardo Contreras <cgcontreras23@gmail.com>
2022-05-09 20:04:06 -0500

    Agregar diccionarios

---
+++ 9 Dictionaries.py
@@ -0,0 +1,42 @@
+product = {
+    "name": "book",
+    "quantity": 3,
+    "price": 123.2
+}
+
+person = {
+    "first name": "Brian",
+    "last name": "Jimenez"
+}
+
+print(
+    type(product)
+)
+
+print(
+    person.keys()
+)
+
+print(
+    person.items()
+)
+
+person.clear
+
+print(
+    person.items()
+)
+
+del person # Eliminar person
+del product
+
+""" print(
+    person.items()
+)
+ """
+products = [
+    {"name": 'Book', "price": 78.4},
+    {"name": 'Encyclopedia', "price": 2178.4}
+]
+
+print(products)
