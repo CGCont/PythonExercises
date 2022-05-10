efb92686c62bf01447f937506f73b66cac1d6f59
Gerardo Contreras <cgcontreras23@gmail.com>
2022-05-09 19:39:15 -0500

    Agregar archivo del tipo de dato tupla

---
+++ 7 Tuples.py
@@ -0,0 +1,12 @@
+x=(1,2,3)
+print(x)
+y = tuple (("a","b","c"))
+print(y)
+tupla = (1,2,3,4,5)
+print(tupla[1])
+del x #Elimina una tupla
+
+locations = {
+    (35.13,12.4) : "Tokyo"
+    (5.13,12.4) : "CDMX"
+}
